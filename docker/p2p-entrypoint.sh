f7c9fbf3669ee73c3a5f70327b224ebce2aa113a
Neil Alexander <neilalexander@users.noreply.github.com>
2020-01-29 12:22:02 +0000

    Riot/Postgres in P2P Dockerfile

--- docker/p2p-entrypoint.sh
+++ docker/p2p-entrypoint.sh
@@ -13,33 +13,6 @@ do
   sleep 1
 done
 
-# Generate the keys if they don't already exist.
-if [ ! -f /etc/dendrite/server.key ] || \
-   [ ! -f /etc/dendrite/server.crt ] || \
-   [ ! -f /etc/dendrite/matrix_key.pem ]; then
-    echo "Generating keys ..."
-
-    rm -f \
-      /etc/dendrite/server.key \
-      /etc/dendrite/server.crt \
-      /etc/dendrite/matrix_key.pem
-
-    test -f /etc/dendrite/server.key || \
-    openssl req -x509 -newkey rsa:4096 \
-      -keyout /etc/dendrite/server.key \
-      -out /etc/dendrite/server.crt \
-      -days 3650 -nodes \
-      -subj /CN=localhost
-
-    test -f /etc/dendrite/matrix_key.pem || \
-    /usr/local/bin/generate-keys \
-      -private-key /etc/dendrite/matrix_key.pem
-fi
-
 # Start nginx
 echo "Starting Riot"
-nginx &
-
-# Start dendrite
-echo "Starting Dendrite"
-cd /etc/dendrite && dendrite-monolith-server
+nginx
